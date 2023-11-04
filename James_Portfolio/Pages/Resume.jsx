1626a26330a8330347e9d35ee078eaeb703df07f
James <joshua.james918@gmail.com>
2023-11-04 15:31:32 -0500

    resume pages

---
+++ James_Portfolio/Pages/Resume.jsx
@@ -0,0 +1,16 @@
+export default function resume () {
+    return (
+        <section>
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-15">
+                        <h2> Resume </h2>
+                        <p>
+                        Some people call me the space cowboy, yeah Some call me the gangster of love, Some people call me Maurice Cause I speak of the pompatus of love a People talk about me baby Say I'm doin' you wrong, doin' you wrong Well, don't you worry, baby, don't worry Cause I'm right here, right here, right here, right here at home Cause I'm a picker I'm a grinner, yadda yadda yadda I play my music in the sun  
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
