230220f1a94dbd66efb2c376cb72cedfa7ba483a
James <joshua.james918@gmail.com>
2023-11-04 20:59:15 -0500

    projects.jsx

--- James_Portfolio/components/Project.jsx
+++ James_Portfolio/components/Project.jsx
@@ -1,11 +1,21 @@
+const Project = ({Title, deployedLink, githubLink}) => {
+    return(
+        <div className="project">
+            <h3>{Title}</h3>
+        {/* // linke to app and github repo */}
 
+        <a href={deployedLink} target="_blank" rel="noopener noreferrer">
+            Deployed link
+        </a>
+        <a href={githubLink} target="_blank" rel="noopener noreferrer">
+            Deployed link
+        </a>
+        </div>
+    );
 
-
-
-
-
-
-
-
-
+}
 export default Project;
+
+
+
+// https://james-professional-portfolio.netlify.app/
