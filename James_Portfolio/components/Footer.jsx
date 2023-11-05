fba409300c06ebede98da6611c8088e72a186d37
James <joshua.james918@gmail.com>
2023-11-04 21:20:22 -0500

    footer

---
+++ James_Portfolio/components/Footer.jsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+    return(
+        <footer className="w-110 mt-auto">
+            <div className="container text-center">
+            {/* link to github */}
+            <a href="https://github.com/JIF945"
+            target="blank"
+            rel="noopener noreferrer"
+            ></a>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
