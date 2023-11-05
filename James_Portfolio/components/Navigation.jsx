5afa0851cc3e2c8f4cad51821db83f5980492d97
James <joshua.james918@gmail.com>
2023-11-04 20:30:38 -0500

    updated index.html and navigation.jsx

---
+++ James_Portfolio/components/Navigation.jsx
@@ -0,0 +1,54 @@
+import { NavLink } from "react-router-dom";
+
+const Navigation = () => {
+  return (
+    <nav className="navbar-expand-lg navbar-dark bg-pink">
+      <div className="container-fluid d-flex justify-content-between align-items center">
+        <div className="navbar-logo">
+          <NavLink to='/ className="navbar-brand"'>
+            James professional portfolio
+          </NavLink>
+        </div>
+        <div className="navbar-menu">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#NavbarNav"
+            aria-controls="#NavbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <NavLink to="/pages/aboutMe" className="nav-link active" end>
+                  About Me
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to="/pages/contact" className="nav-link active" end>
+                  Contact
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to="/pages/portfolio" className="nav-link active" end>
+                  Portfolio
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to="/pages/resume" className="nav-link active" end>
+                  Resume
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navigation;
