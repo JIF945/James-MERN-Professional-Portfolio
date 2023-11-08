93647b197862d97878383172e45af7350dbbfead
James <joshua.james918@gmail.com>
2023-11-07 19:07:49 -0600

    about me section

--- Pages/AboutMe.jsx
+++ Pages/AboutMe.jsx
@@ -7,7 +7,7 @@ function AboutMe(){
                     <div className="container-hide">
                     <h2> About me </h2>
                     <p>
-                    i've been studying web design on my own for one year, i felt my own studying was lacking so i wanted to get formal training. what drew me to web design is the creative problem-solving, the fast-paced environment, and the continuous innovation. I want to expand on my skills so one day i can be a freelance proficient web developer, that travels the world.
+                    i've been studying web design on my own for one year, i felt my own studying was lacking so i wanted to get formal training. what drew me to web design is the creative problem-solving, the fast-paced environment, and the continuous innovation. I want to expand on my skills so one day i can be a freelance proficient web developer, that travels the world. and makes simple websites. 
                     </p>
                     </div>
                 </div>
